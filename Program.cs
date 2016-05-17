42188582de9d5c9381efd93475612afa66f9366e
anheca <anheca68@hotmail.com>
2016-05-16 19:40:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOneStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+            int instancia = 0;
+            var cronometro = new Stopwatch();
+            GetInstructions();
+
+            var colorFuente = Console.ForegroundColor;
+
+            do
+            {
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                keyInfo = Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                if (keyInfo.Key == ConsoleKey.Escape) break;
+
+                if (keyInfo.Key == ConsoleKey.S)
+                {
+                    instancia++;
+
+                    if (instancia > 1)
+                        GetInstructions();
+
+                    try
+                    {
+                        cronometro.Start();
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: {0}", e.Message);
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.P)
+                {
+                    try
+                    {
+                        var duracion = cronometro.Stop();
+                        GetDuration(duracion);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: {0}", e.Message);
+                    }
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Enter);
+
+
+        }
+
+        private static void GetDuration(TimeSpan duracion)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                         " + "Duracion :        Dias........: {0}", duracion.Days);
+            Console.WriteLine("                         " + "                  Horas.......: {0}", duracion.Hours);
+            Console.WriteLine("                         " + "                  Minutos.....: {0}", duracion.Minutes);
+            Console.WriteLine("                         " + "                  Segundos....: {0}", duracion.Seconds);
+            Console.WriteLine("                         " + "                  Milisegundos: {0}", duracion.Milliseconds);
+        }
+
+        private static void GetInstructions()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("                    WELCOME to STOPWATCH System");
+            Console.WriteLine();
+            Console.WriteLine("                    INSTRUCCIONES:");
+            Console.WriteLine("                    [S]   = Iniciar Cronometro");
+            Console.WriteLine("                    [P]   = Parar   Cronometro");
+            Console.WriteLine("                    [ESC] = SALIR");
+            Console.WriteLine("=====================================================================");
+            Console.WriteLine("Please Press any key as above:");
+
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExerciseOneStopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _starStopwatch { get; set; }
+        private DateTime _endStopwatch { get; set; }
+        private bool _isRunning = false;
+
+        public void Start()
+        {
+            if (_isRunning)
+                throw new ArgumentException("The STOPWATCH is currently running, please end the first instance [P].");
+
+            _isRunning = true;
+            _starStopwatch = DateTime.Now;
+
+           
+            Console.WriteLine("\nINICIO.....: {0: H:mm:ss}", _starStopwatch); // just to see the current time to compare visually.
+
+        }
+
+        public TimeSpan Stop()
+        {
+            if (!_isRunning)
+                throw new ArgumentException("Please start the STOPWACTH before [S].");
+
+            _isRunning = false;
+            _endStopwatch = DateTime.Now;
+
+            Console.WriteLine("\nFIN........: {0: H:mm:ss}", _endStopwatch);
+            return (_endStopwatch - _starStopwatch);
+        }
+
+    }
+}
